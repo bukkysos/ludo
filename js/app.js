757fd390f9a42e0dd7acd6f13aabdff877d62f8b
bukkysos <chukwuebukaanyanwusos@gmail.com>
2021-11-09 08:56:16 +0100

    Added dice and dice assets

--- js/app.js
+++ js/app.js
@@ -454,6 +454,34 @@ function handleMultiplePalleteDisplay(palletePath) {
 document.body.onload = startGame;
 
 
+/*
+
+New Roll Dice function
+
+function rollDice() {
+    const dice = [...document.querySelectorAll(".die-list")];
+    dice.forEach(die => {
+      toggleClasses(die);
+      die.dataset.roll = getRandomNumber(1, 6);
+    });
+  }
+  
+  function toggleClasses(die) {
+    die.classList.toggle("odd-roll");
+    die.classList.toggle("even-roll");
+  }
+  
+  function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  document.getElementById("roll-button").addEventListener("click", rollDice);
+  
+
+*/
+
 
 
 // const speak = (msg) => {
